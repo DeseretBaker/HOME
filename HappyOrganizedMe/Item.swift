cd6e7b8eea28903a1f3b9f7b8f7fcdfa52df90fd
Deseret Baker <bakdes72@gmail.com>
2024-07-25 08:16:44 -0600

    Initial Commit

---
+++ HappyOrganizedMe/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HappyOrganizedMe
+//
+//  Created by Deseret Baker on 7/25/24.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ HappyOrganizedMeTests/HappyOrganizedMeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HappyOrganizedMeTests.swift
+//  HappyOrganizedMeTests
+//
+//  Created by Deseret Baker on 7/25/24.
+//
+
+import Testing
+@testable import HappyOrganizedMe
+
+struct HappyOrganizedMeTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
